0009a30591ef6136a4ecad4654e427e5db0d977c
lovetostrike <lovetostrikep@gmail.com>
2018-04-17 03:29:25 -0400

    semantic ui setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+import { PLATFORM } from 'aurelia-pal';
+import { viewResources } from "aurelia-framework";
+
+@viewResources(PLATFORM.moduleName('semantic-ui-css/semantic.min'))
 export class App {
   message = 'Hello World!';
 }
