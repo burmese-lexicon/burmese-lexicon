34d3d01b215f82ccf9c31cebef93458c7e12e498
lovetostrike <lovetostrikep@gmail.com>
2018-04-17 09:14:57 -0400

    fun useless icons

--- src/app.ts
+++ src/app.ts
@@ -18,21 +18,33 @@ export class App {
         name: 'home',
         moduleId: PLATFORM.moduleName('./resources/elements/home-page'),
         nav: true,
-        title: 'Burmese Lexicon'
+        title: 'Burmese Lexicon',
+        settings: {
+          icon: 'book',
+          iconColor: 'red'
+        }
       },
       {
         route: 'words',
         name: 'words',
         moduleId: PLATFORM.moduleName('./resources/elements/words-page'),
         nav: true,
-        title: 'Words'
+        title: 'Words',
+        settings: {
+          icon: 'list',
+          iconColor: 'green'
+        }
       },
       {
         route: 'contributors',
         name: 'contributors',
         moduleId: PLATFORM.moduleName('./resources/elements/contributors-page'),
         nav: true,
-        title: 'Top Contributors'
+        title: 'Top Contributors',
+        settings: {
+          icon: 'trophy',
+          iconColor: 'gold'
+        }
       },
     ]);
   }
