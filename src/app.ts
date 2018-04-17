bf14ae0c024e618d7c6c6cd8571b9ee24152a484
lovetostrike <lovetostrikep@gmail.com>
2018-04-17 04:47:33 -0400

    routing setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import { PLATFORM } from 'aurelia-pal';
+import {Router, RouterConfiguration} from 'aurelia-router';
 import { viewResources } from "aurelia-framework";
 
 @viewResources(PLATFORM.moduleName('semantic-ui-css/semantic.min'))
 export class App {
-  message = 'Hello World!';
+  private router: Router
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('./resources/elements/word-search'), nav: true, title: 'Burmese Lexicon' },
+      { route: 'words', name: 'words', moduleId: PLATFORM.moduleName('./resources/elements/word-search'), nav: true, title: 'Words' },
+      { route: 'contributors', name: 'contributors', moduleId: PLATFORM.moduleName('./resources/elements/word-search'), nav: true, title: 'Top Contributors' },
+    ]);
+  }
 }
---
+++ src/resources/elements/menu-bar.ts
@@ -0,0 +1,5 @@
+import { bindable } from 'aurelia-framework'
+import { Router } from 'aurelia-router'
+export class MenuBar {
+  @bindable router: Router
+}
