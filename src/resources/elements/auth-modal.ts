e8dad276a230e9e1635ab1c29665347eaded121c
lovetostrike <lovetostrikep@gmail.com>
2018-04-22 07:45:49 -0400

    auth modal

---
+++ src/resources/elements/auth-modal.ts
@@ -0,0 +1,18 @@
+import { AuthService } from 'services/auth-service'
+import { autoinject } from 'aurelia-dependency-injection'
+import { EventAggregator } from 'aurelia-event-aggregator'
+
+@autoinject
+export class AuthModal {
+  private modalRef: Element
+  constructor (private ea: EventAggregator, private authService: AuthService) {
+    ea.subscribe(AuthRequestedMessage, this.showModal.bind(this))
+  }
+
+  showModal () {
+    jQuery(this.modalRef).modal('show')
+    this.authService.renderProvidersToContainer(this.modalRef, () => jQuery(this.modalRef).modal('hide'))
+  }
+}
+
+export class AuthRequestedMessage {}
