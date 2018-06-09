8322b0a59ef51c8ff0e6d295203af859b64f00bf
lovetostrike <lovetostrikep@gmail.com>
2018-06-09 10:14:47 -0400

    avoid reloading on selecting word search results

--- src/resources/elements/word-search.ts
+++ src/resources/elements/word-search.ts
@@ -1,25 +1,26 @@
 import { WordsApi } from 'api/words-api'
 import { autoinject } from 'aurelia-framework'
+import { Router } from 'aurelia-router'
 
 @autoinject
 export class WordSearch {
   private words: object[]
 
-  constructor (private element: Element, private wordsApi: WordsApi) {}
+  constructor (private element: Element, private wordsApi: WordsApi, private router: Router) {}
 
   async attached () {
     try {
       this.words = await this.wordsApi.getWordList()
       this.words = this.words.map(word => ({
-        title: word,
-        url: `/words/${word}`
+        title: word
       }))
       jQuery(this.element).find('.ui.search')
         .search({
           searchDelay: 500,
           source: this.words,
-          onSelect: () => {
+          onSelect: (result) => {
             jQuery('.sidebar').sidebar('hide')
+            this.router.navigate(`/words/${result.title}`)
           }
           // TODO: will need this when we do proper full text search
           // apiSettings: {
