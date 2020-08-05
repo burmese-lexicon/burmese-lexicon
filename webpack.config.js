0e884a3a0a4e3e93711b30b780de768635eefc51
lovetostrike <lovetostrikep@gmail.com>
2020-08-05 00:04:52 -0400

    update readme and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const when = (condition, config, negativeConfig) =>
   condition ? ensureArray(config) : ensureArray(negativeConfig);
 
 // primary config:
-const title = 'Aurelia Navigation Skeleton';
+const title = 'Burmese Lexicon';
 const outDir = path.resolve(__dirname, project.platform.output);
 const srcDir = path.resolve(__dirname, 'src');
 const nodeModulesDir = path.resolve(__dirname, 'node_modules');
